fb9d9e89661790c40ca2b8ea1b199b279d89ceb3
AmalNambiar12 <f20212670@goa.bits-pilani.ac.in>
2024-01-21 08:40:41 +0530

    Introduced a components folder and a new component

---
+++ client/src/components/FileChooser.js
@@ -0,0 +1,12 @@
+import styles from './FileChooser.module.css'; 
+
+const FileChooser = () => {
+  return (
+    <div className={styles.file_chooser}>
+        hihsi
+        <br /><input type="file"></input>
+    </div>
+  );
+}
+
+export default FileChooser;
--- client/src/pages/Processing.js
+++ client/src/pages/Processing.js
@@ -1,4 +1,5 @@
 import styles from './Processing.module.css'; 
+import FileChooser from '../components/FileChooser';
 
 const Processing = () => {
   return (
@@ -6,11 +7,8 @@ const Processing = () => {
         <span id="textbox" className={styles.textbox}>HELLO WORLD</span>
         <div id="process_box" className={styles.process_box}>
             <div className={styles.dual_box}>
-                <div className={styles.file_chooser}>
-                    hihsi
-                    <br /><input type="file"></input>
-                </div>
-                <div className={styles.file_chooser}>hihsi</div>
+                <FileChooser />
+                <FileChooser />
             </div>
             <button className={styles.submit_button}>Process</button>
         </div>
